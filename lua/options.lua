2c36745cda8c2a2244be0219454f472e3f851ccb
krzychhoo <krzychhoo@gmail.com>
2024-04-03 09:35:47 +0200

    Set pwsh as the default shell on Windows

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,14 @@ require "nvchad.options"
 
 -- add yours here!
 --
+if vim.fn.has("win32") == 1 then
+    vim.o.shell = "pwsh"
+    vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.o.shellquote = ""
+    vim.o.shellxquote = ""
+end
+-- vim.opt.shellcmdflag = "-c ls"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
