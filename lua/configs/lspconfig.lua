319469c61bc679a4f493d0322c7d8ee576c2a961
krzychhoo <130290978+krzychhoo@users.noreply.github.com>
2024-04-18 16:34:21 +0200

    LSP + conform cleanup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,15 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "rust_analyzer" }
+local servers = {
+  -- "html",
+  -- "cssls",
+  "clangd",
+  "rust_analyzer",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +20,9 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- -- typescript
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
